09445e9b8a40696c89841dab38992da02eba9c99
Mehedi-Hassan <mehedikazi812@gmail.com>
2024-08-12 14:43:21 +0600

    added order controler and create getorderOfUser method for retrive order

---
+++ Library_managment_system_API/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using Library_managment_system_API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library_managment_system_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        public OrderController(ContextDb contextDb)
+        {
+            ContextDb = contextDb;
+        }
+
+        public ContextDb ContextDb { get; }
+
+        [Authorize]
+        [HttpGet("GetOrderOfUser")]
+        public IActionResult GetOrderOfUser(int userId)
+        {
+            var orders = ContextDb.Orders
+                .Include(o => o.Book)
+                .Include(o => o.User)
+                .Where(o => o.UserId == userId).ToList();
+
+            if(orders.Any())
+            {
+                return Ok(orders);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
